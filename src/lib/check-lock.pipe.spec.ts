f7c3cc29eda06fbdeefa7c8c313e55f821f3ed70
Steffo Dimfelt <steffo.dimfelt@gmail.com>
2023-12-08 12:22:13 +0100

    1.0.8: Adjust locked days and selected date

--- src/lib/check-lock.pipe.spec.ts
+++ src/lib/check-lock.pipe.spec.ts
@@ -1,8 +1,10 @@
-import { CheckLockPipe } from './check-lock.pipe';
+import { CheckLockPipe } from "./check-lock.pipe";
+import { DomSanitizer } from "@angular/platform-browser";
+import { inject, TestBed } from "@angular/core/testing";
 
-describe('CheckLockPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CheckLockPipe();
+describe("CheckLockPipePipe", () => {
+  it("create an instance", inject([DomSanitizer], (domSanitizer: any) => {
+    const pipe = new CheckLockPipe(domSanitizer);
     expect(pipe).toBeTruthy();
-  });
+  }));
 });
